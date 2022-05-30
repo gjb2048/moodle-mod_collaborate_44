a355f23a540b030084787efe67584b5a8c163f2d
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2022-05-30 17:16:37 +0100

    Week 6 Something I noticed.

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborate';
-$plugin->version = 2024051809;
+$plugin->version = 2024051810;
 $plugin->requires = 2024042200.00; // 4.4 (Build: 20240422).
 $plugin->supported = [404, 404];
-$plugin->release = '404.1.5';
+$plugin->release = '404.1.6';
 $plugin->maturity = MATURITY_STABLE;
